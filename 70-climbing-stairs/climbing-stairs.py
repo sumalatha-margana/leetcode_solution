7ea5b59e5efaa132664dadb0bc26d16a28f3f8f9
Sumalatha Margana <sumalathamargana@gmail.com>
2026-09-11 21:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (86.81%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 1
+
+        for i in range(n):
+            c = a + b
+            a = b
+            b = c
+
+        return a
+        
