f4f31c967798824b58f9480c25a809ebb722a130
Sumalatha Margana <sumalathamargana@gmail.com>
2026-09-18 09:32:52 +0530

    Time: 55 ms (5.27%) | Memory: 19.2 MB (45.60%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.py
@@ -0,0 +1,16 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        result = ""
+        i = 0
+        while i < len(command):
+            if command[i] == 'G':
+                result += 'G'
+                i += 1
+            elif command[i:i+2] == "()":
+                result += 'o'
+                i += 2
+            else:
+                result += "al"
+                i += 4
+        return result
+        
